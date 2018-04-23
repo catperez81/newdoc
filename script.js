00d5c639905b875421917dcff7273bf68979ead7
catperez81 <catperez81@gmail.com>
2018-04-23 10:58:47 -0700

    js work

--- script.js
+++ script.js
@@ -8,9 +8,34 @@ function submitForm() {
   });
 }
 
+/* template to pass through each question from the object */
+function buildTemplate(question) {
+  $("#question-text").text(question.questionText);
+  let answers = question.questionOptions.map((currentQuestion, index) => {
+    return `
+    <label id="label-${index}">
+      <input type="radio" name="badass-woman" class="question-input" value="${index}" required>
+      ${currentQuestion}
+    </label>
+    <br>`;
+  });
+
+  $("#answer-group").html(answers);
+}
+
+/* render new question */
+function renderNewQuestion() {
+  $(".current-question").text(state.currentQuestionIndex + 1);
+  $(".question-list").html(
+    buildTemplate(storeQuestions.questions[state.currentQuestionIndex])
+  );
+  $(".question-input").click(checkAnswer);
+  $("#quiz-question, #quiz-tracking").show("slow");
+}
+
 /* render search results */
 function doctorList() {
-  
+
 }
 
 /* render doctor profile */
