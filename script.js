932c5caac4ad8f35c5981e643290bb8f2f159561
catperez81 <catperez81@gmail.com>
2018-04-26 08:41:47 -0700

    api working

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 // const BETTERDOCTOR_SEARCH_URL = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=38a5e05a1ba6c75134d6d9a0497c51c0';
+const BETTERDOCTOR_SEARCH_URL = 'https://api.betterdoctor.com/2016-03-01/doctors'
 
 
-// function getDataFromApi(name, callback) {
-//   const settings = {
-//     url: BETTERDOCTOR_SEARCH_URL,
-//     data: [
-//     {
-//       [
-//         "visit_address": {
-//           "city": "",
-//           "state_long": "",
-//           "street": "",
-//           "zip": ""
-//         },
-//       ]
-//     },
-//       "meta": {
-//         "data_type": "array",
-//         "item_type": "Doctor",
-//         "count": ,
-//         "limit": 
-//       },
-//     dataType: 'json',
-//     type: 'GET',
-//     success: callback
-//   };
+function getDataFromApi(lat,lng) {
+  const settings = {
+    url: BETTERDOCTOR_SEARCH_URL,
+    data: { 
+      location: `${lat}, ${lng}, 100`,
+      skip: 0,
+      limit: 25,
+      user_key: '38a5e05a1ba6c75134d6d9a0497c51c0'
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: function(response) {
+      results(response.data);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  };
 
-//   $.ajax(settings);
-// }
+  $.ajax(settings);
+}
 
-function renderDoc(result) {
-  console.log(result);
+/* Pass through all results */
+function results(doctors){
+  // map results here //
+  const results = doctors.map((item, index) => renderDoctor(item));
+  $('#doc-results').html(results);
+  console.log(doctors);
+}
+
+/* Pass through each single result */
+function renderDoctor(doctor) {
+  return `
+    <div>
+      <h3>${doctor.profile.first_name}</h3>
+      <h3>${doctor.profile.last_name}</h3>
+    </div>
+    <br>`
 }
 
 function logoClickable(){
@@ -41,44 +50,31 @@ function logoClickable(){
   });
 }
 
-// function displayBetterDoctorSearchData(data) {
-//   const results = data.items.map((item, index) => renderResult(item));
-//   $('#doc-results').html(results);
-// }
-
 /* submit doctor search form */
 function submitForm() {
-  $("#findadoc").on("submit", function() {
+  $("#findadoc").submit(function(event) {
     event.preventDefault();
-    // let nameInput = $('#name').val();
-    // console.log('This variable is', nameInput);
-    // let zipCode = $('#zip').val();
+    console.log('test');
+    let zipCode = $('#zip').val();
+    $('#zip').val('');
     // let healthPlan = $('#plan-dropdown').val();
     // let specialty = $('#specialty-dropdown').val();
     $("#doc-search-form").hide();
     $("#doc-results").show();
     // var docs = result.map(doc=>renderDoc())
     // $("#doc-results").html(docs);
-    // getDataFromApi(name, callback);
+    getLatLong(zipCode);
   });
 }
 
+console.log('random');
+
+function getLatLong(zipCode) {
+ // get lat lng from Google Maps // 
+  getDataFromApi(37.755117, -122.457847);
+}
+
 submitForm();
-
-
-/* Pass through test results array */
-// function renderDoc(result) {
-//   let results = results.doctorResult;
-//   $(".doctor-list").html( `
-//     <div>
-//       <h3>${doctorResult.doctorName}</h3>
-//       <p>${doctorResult.address}<p>
-//       <p>${doctorResult.distance}<p>
-//     </div>
-//     <br>`
-//   );
-// }
-
 
 /* restart the search */
 function newDoctorSearch() {
